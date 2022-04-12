0836fafd27a0d2943b8ac788b3cde292c4005bcd
emmiiorji <emmanuelorji2013@gmail.com>
2022-04-12 17:46:32 +0100

    Link menu items to their sections

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-function toggleOpenMenu() {
-  document.querySelector('header .toolbar').classList.toggle('show-mobile-menu');
-}
-
 const hamburger = document.getElementById('hamburger');
 const closeMenu = document.querySelector('#mobile-menu .close');
+const menuNavs = Array.from(document.querySelectorAll('.toolbar #nav-menu li'));
 
-hamburger.addEventListener('click', () => {
-  toggleOpenMenu();
-  hamburger.style.display = 'none';
-  closeMenu.style.display = 'block';
-});
+function toggleOpenMenu() {
+  document.querySelector('header .toolbar').classList.toggle('show-mobile-menu');
+  if (window.getComputedStyle(hamburger).display === 'none') {
+    hamburger.style.display = 'inline';
+  } else {
+    hamburger.style.display = 'none';
+  }
+  if (window.getComputedStyle(closeMenu).display === 'none') {
+    closeMenu.style.display = 'block';
+  } else {
+    closeMenu.style.display = 'none';
+  }
+}
 
-closeMenu.addEventListener('click', () => {
-  toggleOpenMenu();
-  hamburger.style.display = 'block';
-  closeMenu.style.display = 'none';
+hamburger.addEventListener('click', toggleOpenMenu);
+
+closeMenu.addEventListener('click', toggleOpenMenu);
+
+menuNavs.forEach((nav) => {
+  nav.addEventListener('click', toggleOpenMenu);
 });
