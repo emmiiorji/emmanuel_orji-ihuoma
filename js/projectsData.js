5e55047ec2ae1317def2f4b80248c7d1c05a478a
emmiiorji <emmanuelorji2013@gmail.com>
2022-04-14 14:46:00 +0100

    Update projects data object

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,7 +1,44 @@
 export default () => [
   {
-    name: 'Lorem ipsum dolor sit amet.',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis eveniet ea possimus minus suscipit, fugit incidunt ipsa architecto, excepturi saepe totam quidem nam cum dignissimos rerum praesentium similique. Officia, perspiciatis.',
-    image: 'https://res.cloudinary.com/emmii/image/upload/v1649836653/my_portfolio/project_screenshot_ctmepz.png',
+    name: 'Multi-Post Stories',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    imageURL: './assets/img/project1-img.png',
+    languages: ['css', 'html', 'Bootstrap', 'Ruby'],
   },
 ];
