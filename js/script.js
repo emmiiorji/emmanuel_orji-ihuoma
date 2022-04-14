8e7c1a83453bb56dcb3d050f1045d082b226dcf8
emmiiorji <emmanuelorji2013@gmail.com>
2022-04-14 14:47:58 +0100

    Add script for opening and closing modals

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const projects = projectsData();
 const hamburger = document.getElementById('hamburger');
 const closeMenu = document.querySelector('#mobile-menu .close');
 const menuNavs = Array.from(document.querySelectorAll('.toolbar #nav-menu li'));
+const projectsSection = document.getElementById('my-works');
 
 function toggleOpenMenu() {
   document.querySelector('header .toolbar').classList.toggle('show-mobile-menu');
@@ -19,10 +20,153 @@ function toggleOpenMenu() {
   }
 }
 
+function createLanguagesUlElement(languages) {
+  let markup = '<ul>';
+  languages.forEach((language) => {
+    markup += `<li>${language}</li>`;
+  });
+  markup += '</ul>';
+  return markup;
+}
+
+const markupModal = (projectId) => {
+  const project = projects[projectId];
+  return `<div class="project-margin">
+            <div class="project-modal-container">
+              <div class="modal-header">
+                <h2>${project.name}</h2>
+                <img src="./assets/img/close-icon.png" class="close" alt="Close Modal">
+              </div>
+              ${createLanguagesUlElement(project.languages)}
+              <div class="project-modal-body">
+                <img src="${project.imageURL}" class="project-img" alt="Project Image">
+                <div class="project-describe">
+                  <p class="description">
+                    ${project.description}
+                  </p>
+                  <div class="points-of-action">
+                    <button type="button">See Live<i class="fa-solid fa-power-off"></i></button>
+                    <button type="button">See Source<i class="fa-brands fa-github"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+};
+
+function toggleOpenModal(e) {
+  const modalElement = document.querySelector('#my-works .project-modal');
+  if (window.getComputedStyle(modalElement).display === 'none') {
+    const projectId = e.target.id.split('_')[1] - 1;
+    modalElement.innerHTML = markupModal(projectId);
+    modalElement.style.display = 'block';
+  } else {
+    modalElement.style.display = 'none';
+  }
+  if (window.getComputedStyle(closeMenu).display === 'none') {
+    closeMenu.style.display = 'block';
+  } else {
+    closeMenu.style.display = 'none';
+  }
+}
+
+//  The leading project is the last entered project
+const markupLeadingProject = () => {
+  if (projects.length < 1) {
+    return '';
+  }
+  const leadingProject = projects[projects.length - 1];
+  const projectId = `project_${projects.length}`;
+  return `<div class="card">
+            <div class="card-img">
+              <img src="${leadingProject.imageURL}" alt="${leadingProject.name}">
+            </div>
+            <div id="leading-card">
+              <h2>${leadingProject.name}</h2>
+              <p>${leadingProject.description}</p>
+              ${createLanguagesUlElement(leadingProject.languages)}
+              <button type="button" class="see-leading-project" id="${projectId}">
+                See Project
+              </button>
+
+            </div>
+          </div>`;
+};
+
+const markupOtherProjects = (projectIndex) => {
+  if (projects.length < 2) {
+    return '';
+  }
+  const project = projects[projectIndex];
+  const projectId = `project_${projectIndex + 1}`;
+  return `<div class="card card-with-bgimage active">
+            <div class="overlay">
+              <div class="shown">
+                <h2>${project.name}</h2>
+                <p>
+                  ${project.description}
+                </p>
+                ${createLanguagesUlElement(project.languages)}
+              </div>
+            </div>
+              <button type="button" class="hidden" id="${projectId}">
+                See Project
+              </button>
+          </div>`;
+};
+
+function markupAllProjects() {
+  let works = '';
+
+  for (let index = projects.length - 1; index >= 0; index -= 1) {
+    let project = '';
+    try {
+      //  Reverse projects. Most recent comes first
+      if ((index === projects.length - 1) && (projects.length > 0)) {
+        project = markupLeadingProject();
+      } else if (projects.length > 1) {
+        project = markupOtherProjects(index);
+      }
+      works += project;
+    } catch (error) {
+      //  Skip the project and do not render it
+    }
+  }
+  //  Returns all project cards
+  return works;
+}
+
+function renderProjects() {
+  let projectsMarkup = '';
+  const projectCards = markupAllProjects();
+  if (projectCards) {
+    projectsMarkup = `<div id="works-header">
+                          <h2>My Recent Works</h2>
+                          <hr>
+                      </div>
+                      <div class="cards-container">
+                        ${projectCards}
+                        <div class="project-modal"></>
+                      </div>`;
+  }
+  projectsSection.innerHTML = projectsMarkup;
+}
+renderProjects();
+
 hamburger.addEventListener('click', toggleOpenMenu);
-
 closeMenu.addEventListener('click', toggleOpenMenu);
-
 menuNavs.forEach((nav) => nav.addEventListener('click', toggleOpenMenu));
 
-console.log(projects);
+const projectButtons = document.querySelectorAll('.works-section .card button');
+
+const modalBlock = document.querySelector('.project-modal');
+modalBlock.addEventListener('click', (e) => {
+  if (e.target.classList.contains('close')) { // From the modal close button
+    toggleOpenModal(e);
+  }
+});
+
+//  Listener for closing the modal is added in HTML
+projectButtons.forEach((button) => {
+  button.addEventListener('click', toggleOpenModal);
+});
