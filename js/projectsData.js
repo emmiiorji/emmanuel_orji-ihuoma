7d6c41f68ca70b8a2a03192050d8603456ff5b27
emmiiorji <emmanuelorji2013@gmail.com>
2022-04-25 14:42:21 +0100

    Modify projects data

--- js/projectsData.js
+++ js/projectsData.js
@@ -53,6 +53,6 @@ export default () => [
     imageURL: 'https://res.cloudinary.com/emmii/image/upload/v1650824023/my_portfolio/pm-global-home_rns1kg.jpg',
     languages: ['HTML', 'CSS', 'Javascript'],
     hostedURL: 'https://emmiiorji.github.io/project-mgt-workshop/',
-    githubURL: 'https://github.com/emmiiorji/project-mgt-workshop',
+    githubURL: 'https://github.com/emmiiorji/my_new_portfolio',
   },
 ];
