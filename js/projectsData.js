f08734ce662d2b9af385a86ad8328ce15c794d78
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-12 04:47:47 +0100

    Add Worldwide Holidays App to projects

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,5 +1,13 @@
 export default () => [
   {
+    name: 'Worldwide Holidays App',
+    description: "This mobile web application developed with React/Redux lets you view global holidays from one point, reading data from the Calendarific's API. The countries' data is loaded from Open Disease.",
+    imageURL: 'https://res.cloudinary.com/emmii/image/upload/v1669138470/my_portfolio/holidays-app_l9pi1u.png',
+    languages: ['React', 'HTML', 'CSS'],
+    hostedURL: 'https://holidays-worldwide.herokuapp.com/',
+    githubURL: 'https://github.com/emmiiorji/holidays-worldwide',
+  },
+  {
     name: 'Scriptmond Leaderboard',
     description: 'Displays scores by different players of a game. It also allows you to submit your score. All data is preserved thanks to an external Leaderboard API service.',
     imageURL: 'https://res.cloudinary.com/emmii/image/upload/v1669112978/my_portfolio/leaderboard_desktop_alordv.png',
