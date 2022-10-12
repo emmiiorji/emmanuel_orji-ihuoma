534e648c807fa1e0733f8cdc7d04abf7c765572f
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-12 04:47:14 +0100

    Add Leaderboard App to projects

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,5 +1,14 @@
 export default () => [
   {
+    name: 'Scriptmond Leaderboard',
+    description: 'Displays scores by different players of a game. It also allows you to submit your score. All data is preserved thanks to an external Leaderboard API service.',
+    imageURL: 'https://res.cloudinary.com/emmii/image/upload/v1669112978/my_portfolio/leaderboard_desktop_alordv.png',
+    imgMobile: 'https://res.cloudinary.com/emmii/image/upload/v1669112978/my_portfolio/leaderboard_mobile_sfnkxr.png',
+    languages: ['HTML', 'CSS', 'Javascript'],
+    hostedURL: 'https://emmiiorji.github.io/leaderboard-list/',
+    githubURL: 'https://github.com/emmiiorji/leaderboard-list',
+  },
+  {
     name: 'Math Magicians App',
     description: 'A Single Page App (SPA) developed with React that allows users to make simple calculations and read a random math-related quote.',
     imageURL: 'https://res.cloudinary.com/emmii/image/upload/v1666193436/my_portfolio/Math-Magician-Desktop_p0qbuw.jpg',
